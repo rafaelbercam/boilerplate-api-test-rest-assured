7927272a62949b870fe05b5447c49d6639ac237d
rafaelbercam <rbercam.medeiros@gmail.com>
2021-06-28 18:45:14 -0300

    add product validations

--- src/test/java/br/com/restassured/data/products/DataProducts.java
+++ src/test/java/br/com/restassured/data/products/DataProducts.java
@@ -1,5 +1,60 @@
 package br.com.restassured.data.products;
 
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
 public class DataProducts {
 
+    private String nome;
+    private String preco;
+    private String descricao;
+    private String quantidade;
+
+
+    public DataProducts getProduct(DataProducts products){
+        Faker faker = new Faker(new Locale("pt-BR"));
+        Long quantidade = faker.number().randomNumber();
+        Integer preco = faker.number().numberBetween(50,500);
+        products.setNome(faker.commerce().productName());
+        products.setPreco(preco.toString());
+        products.setDescricao(faker.commerce().material());
+        products.setQuantidade(quantidade.toString());
+
+        return products;
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
 }
---
+++ src/test/java/br/com/restassured/test/products/Products.java
@@ -0,0 +1,66 @@
+package br.com.restassured.test.products;
+
+import br.com.restassured.commons.RequestSpecificationSetup;
+import br.com.restassured.requests.login.LoginRequests;
+import br.com.restassured.requests.products.ProductsRequests;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
+
+public class Products extends RequestSpecificationSetup {
+    private final ProductsRequests request = new ProductsRequests();
+    private final LoginRequests login = new LoginRequests();
+    private static Response response;
+    private static String token;
+    private static String _id;
+
+
+    @Before
+    public void ScenarioSetup(){
+        Response loginToken = login.postLoginSuccess();
+        token = loginToken.getBody().jsonPath().get("authorization");
+
+        Response idProduct = request.postProduct(token);
+        _id = idProduct.getBody().jsonPath().get("_id");
+    }
+
+    @Test
+    public void getProducts(){
+        response = request.getProducts();
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
+    }
+
+    @Test
+    public void postNewProduct(){
+        response = request.postProduct(token);
+        assertEquals(HttpStatus.SC_CREATED, response.statusCode());
+        assertEquals("Cadastro realizado com sucesso", response.getBody().jsonPath().get("message"));
+    }
+
+    @Test
+    public void getProductById(){
+        response = request.getProductById(_id);
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
+    }
+
+    @Test
+    public void editProduct(){
+        response = request.editProduct(token, _id);
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
+        assertEquals("Registro alterado com sucesso", response.getBody().jsonPath().get("message"));
+    }
+
+    @Test
+    public void deleteProduct(){
+        Response deleteProduct = request.postProduct(token);
+        String _id = deleteProduct.getBody().jsonPath().get("_id");
+        response = request.deleteProduct(token, _id);
+        assertEquals(HttpStatus.SC_OK, response.statusCode());
+        assertEquals("Registro excluído com sucesso", response.getBody().jsonPath().get("message"));
+    }
+}
